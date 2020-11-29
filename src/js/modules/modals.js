10b208ec2546f424b56b52e5c7386d8175894668
Andrey Sergeev <andreyinoks93@yandex.ru>
2020-11-29 18:19:27 +0700

    animation, scroll window  fixed

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,7 +4,8 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = modal.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]')
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll()
 
         trigger.forEach(item => {
             item.addEventListener('click', (evt) => {
@@ -18,6 +19,7 @@ const modals = () => {
 
                 modal.style.display = 'block'
                 document.body.style.overflow = 'hidden'
+                document.body.style.marginRight = `${scroll}px`
                 // document.body.classList.add('modal-open')
             })
         })
@@ -30,6 +32,7 @@ const modals = () => {
 
             modal.style.display = 'none'
             document.body.style.overflow = 'auto'
+            document.body.style.marginRight = `0px`
             // document.body.classList.remove('modal-open')
         })
 
@@ -42,6 +45,7 @@ const modals = () => {
 
                 modal.style.display = 'none'
                 document.body.style.overflow = 'auto'
+                document.body.style.marginRight = `0px`
                 // document.body.classList.remove('modal-open')
             }
         })
@@ -52,6 +56,21 @@ const modals = () => {
             document.querySelector(selector).style.display = 'block'
             document.body.style.overflow = 'hidden'
         }, time)
+    }
+
+    const calcScroll = () => {
+        let div = document.createElement('div')
+        div.style.width = '50px'
+        div.style.height = '50px'
+        div.style.overflowY = 'scroll'
+        div.style.visibility = 'hidden'
+
+        document.body.appendChild(div)
+
+        let scrollWidth = div.offsetWidth - div.clientWidth
+        div.remove()
+
+        return scrollWidth
     }
 
     bindModals('.popup_engineer_btn', '.popup_engineer', '.popup_close')
